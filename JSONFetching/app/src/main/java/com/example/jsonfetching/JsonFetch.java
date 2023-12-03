8f7553065571419166876dd6756b3e8b97ee3e15
priyanshnihalani-169 <priyansh.nihalani@gmail.com>
2023-12-03 19:47:30 +0530

    Android

---
+++ JSONFetching/app/src/main/java/com/example/jsonfetching/JsonFetch.java
@@ -0,0 +1,2 @@
+package com.example.jsonfetching;public class JsonFetch {
+}
--- NetworkAPI/app/src/main/java/com/example/networkapi/MainActivity.java
+++ NetworkAPI/app/src/main/java/com/example/networkapi/MainActivity.java
@@ -4,11 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
+    TextView list;
+//    ListView view;
+    //    /todos/
+    String api = "https://jsonplaceholder.typicode.com/todos/1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        RequestQueue requestQueue;
+        requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, api, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.d("Data", "MyData " + response.getString("title"));
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error", "Someting is wrong");
+                    }
+                });
+        requestQueue.add(jsonObjectRequest);
+        }
 }
